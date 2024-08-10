60634b6247b0575e2d521e65f3b553a2f18e0db2
Mordr3d64 <derrick_sunglao@dlsu.edu.ph>
2024-08-11 00:01:47 +0800

    current progress

--- src/main/java/com/example/studymatch/HelloApplication.java
+++ src/main/java/com/example/studymatch/HelloApplication.java
@@ -51,7 +51,7 @@ public class HelloApplication extends Application {
 
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 520, 380);
         stage.setScene(scene);
         stage.show();
     }
